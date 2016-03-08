d5a63906edcf5e88cd3e48b524e5af12e3075c20
Pedro Semeano <semeano@gmail.com>
2016-03-08 19:21:21 +0000

    Show information on the view. Added weather icons. Fix gulp tasks.

--- app/data/weather.srv.js
+++ app/data/weather.srv.js
@@ -4,13 +4,21 @@
 
 	angular.module('compucorp.data')
 
-		.constant('API', 'http://api.openweathermap.org/data/2.5/weather?')
-		.constant('APP_ID', '6da42917120e3a188b8f0a6efed30502')
+		.constant('API', { url: 'http://api.openweathermap.org/data/2.5/weather?',
+											 appid: '6da42917120e3a188b8f0a6efed30502',
+											 options: '&units=metric' })
+
+		.run(function ($rootScope, $http) {
+			$http({ method: 'GET', url: 'icons.json' })
+				.then(function (data) {
+					$rootScope.icons = data;
+				});
+		})
 
 		.factory('WeatherSrv', WeatherSrv);
 
 
-	function WeatherSrv($http, API, APP_ID) {
+	function WeatherSrv($http, API) {
 
 		var service =  { get: get };
 
@@ -18,7 +26,7 @@
 
 		// Get project
 		function get(query) {
-			return $http({ method: 'GET', url: API + 'appid=' + APP_ID + '&' + query });
+			return $http({ method: 'GET', url: API.url + 'appid=' + API.appid + '&' + query + API.options });
 		}
 
 		return service;
